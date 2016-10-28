50b47223fcfb16c0b38ce5e57a6f0e91f953ad07
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-10-28 21:55:44 +0200

    build shared library

---
+++ deps/build.jl
@@ -0,0 +1,14 @@
+cd(dirname(@__FILE__))
+
+if (!ispath("lib"))
+    run(`mkdir lib`)
+end
+
+download("http://www.maths.uq.edu.au/expokit/expokit.tar.gz", "expokit.tar.gz")
+run(`tar xzvf expokit.tar.gz`)
+
+cd(joinpath(dirname(@__FILE__), "expokit", "fortran" ))
+run(`make -f ../../Makefile1`)
+
+run(`mv libexpokit.$(Libdl.dlext) ../../lib`)
+
